8dd07c57757518a3a33af7717b373fa078bfa0aa
Darkphoenix <voidlucario173@gmail.com>
2025-10-07 10:29:42 +0530

    Remove all Geist font dependencies to fix Netlify build issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
